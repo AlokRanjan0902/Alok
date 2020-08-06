2f91c860bd0352ebb6d70305ad92b3cf770d64f6
Alok Ranjan <Alok Ranjan@AlokRanjan-VAIO>
2020-08-06 17:37:32 +0530

    commit

---
+++ src/test/java/com/acttitime/Timetrack/Demo.java
@@ -0,0 +1,14 @@
+package com.acttitime.Timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+@Test
+	
+	public void run() {
+		Reporter.log("sucessful run",true);
+	}
+
+}
